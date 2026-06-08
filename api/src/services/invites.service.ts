628cc3c12888b8d5930f7139c4c6cf18b8fdee0a
Dimitrios-Spiridon Karampelas <24dimitris@gmail.com>
2026-06-08 20:23:44 +0300

    Added invites service to allow users to invite each other to different lists

---
+++ api/src/services/invites.service.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const sendInvite = async (
+  listId: string,
+  inviterId: string,
+  inviteeEmail: string,
+  role: "VIEWER" | "MEMBER" = "VIEWER",
+) => {
+  const user = await prisma.listMember.findUnique({
+    where: { userId_listId: { userId: inviterId, listId } },
+  });
+  if (!user || user.role !== "OWNER") throw new Error("Authorization Error");
+  const invitee = await prisma.user.findUnique({
+    where: { email: inviteeEmail },
+  });
+  if (!invitee) throw new Error("User Error");
+  const invite = await prisma.invite.create({
+    data: {
+      listId,
+      inviterId,
+      inviteeId: invitee.id,
+      role,
+    },
+  });
+  return invite;
+};
+
+export const respondToInvite = async (
+  inviteId: string,
+  userId: string,
+  response: "ACCEPTED" | "REJECTED",
+) => {
+  const invite = await prisma.invite.findUnique({ where: { id: inviteId } });
+  if (!invite || invite.inviteeId !== userId)
+    throw new Error("Authorization Error");
+  if (invite.status !== "PENDING")
+    throw new Error("Invite already responded to");
+  const updatedInvite = await prisma.invite.update({
+    data: { status: response },
+    where: { id: inviteId },
+  });
+  if (response === "ACCEPTED") {
+    await prisma.listMember.create({
+      data: {
+        userId,
+        listId: invite.listId,
+        role: invite.role,
+      },
+    });
+  }
+  return updatedInvite;
+};
+
+export const getInvites = async (userId: string) => {
+  const invites = await prisma.invite.findMany({
+    where: { inviteeId: userId, status: "PENDING" },
+    include: { list: true },
+  });
+  return invites;
+};
